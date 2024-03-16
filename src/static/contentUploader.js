3a7c971225b73f68c92b0f338f3e4c6e5e4279e2
Nick Biryulin <keddad@yandex.ru>
2024-03-16 22:03:06 +0300

    Bunch of UI work

--- src/static/contentUploader.js
+++ src/static/contentUploader.js
@@ -1,5 +1,6 @@
 telegramData = {}
 sourceToMessage = {}
+nameToSource = {}
 
 function parseBasicInfo() {
     const output = document.getElementById('basicInfo');
@@ -16,8 +17,23 @@ function parseBasicInfo() {
     nameElem = document.createElement("li");
     nameElem.textContent = `Message sources: ${Object.keys(sourceToMessage).length}`;
     output.appendChild(nameElem);
+}
 
-    output.style = ["display"];
+async function removeTextInput() {
+    const inputDiv = document.getElementById("user-select-textinputs");
+    
+    if (inputDiv.children.length != 0) {
+        inputDiv.children[inputDiv.children.length - 1].remove();
+    }
+}
+
+async function addTextInput() {
+    const inputDiv = document.getElementById("user-select-textinputs");
+
+    const newTextElem = document.createElement("input");
+    newTextElem.setAttribute("list", "user-names");
+
+    inputDiv.appendChild(newTextElem);
 }
 
 async function handleFileUpload(event) {
@@ -25,20 +41,38 @@ async function handleFileUpload(event) {
     telegramData = JSON.parse(await file.text());
     sourceToMessage = {}
 
-    for (chat in telegramData["chats"]["list"]) {
+    for (chat of telegramData["chats"]["list"]) {
         for (message of (chat["messages"] || [])) {
             if ("from_id" in message) {
-                source = message["from_id"]
+                source = message["from_id"];
 
                 if (source in sourceToMessage) {
-                    sourceToMessage[source] += [message]
+                    sourceToMessage[source] += [message];
                 } else {
-                    sourceToMessage[source] = [message]
+                    sourceToMessage[source] = [message];
+                }
+
+                source_name = message["from"];
+
+                if (!(source_name in nameToSource)) {
+                    nameToSource[source_name] = source;
                 }
             }
         }
     }
 
+    const datalist = document.getElementById("user-names")
+    datalist.innerHTML = "";
+
+    for (source of Object.keys(nameToSource)) {
+        option = document.createElement("option");
+        option.value = source;
+
+        datalist.appendChild(option);
+    }
 
     parseBasicInfo()
+
+    const analyzeDiv = document.getElementById("user-select");
+    analyzeDiv.style = "";
 }
